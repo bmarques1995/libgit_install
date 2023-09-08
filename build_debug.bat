cf542a21b18ef58cb10ba70e9cf77408debf9464
B Marques <brunosmarques1995@hotmail.com>
2023-09-08 18:14:52 -0300

    Added libssh2

--- build_debug.bat
+++ build_debug.bat
@@ -22,5 +22,8 @@ nmake install
 
 cd ../../
 
+cmake -S ./modules/libssh2 -B ./dependencies/libssh2 -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS=ON
+cmake --build ./dependencies/libssh2 --config release --target install
+
 cmake -S ./modules/libgit2 -B ./dependencies/libgit2 -DCMAKE_INSTALL_PREFIX="./install" -DBUILD_SHARED_LIBS=ON
 cmake --build ./dependencies/libgit2 --config release --target install
